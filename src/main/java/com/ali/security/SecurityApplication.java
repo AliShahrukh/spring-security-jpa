80ee297aefb8e809101ec084f25082dc342248c9
Shahrukh <37803285+AliShahrukh@users.noreply.github.com>
2020-03-24 21:43:49 +0530

    Add files via upload

---
+++ src/main/java/com/ali/security/SecurityApplication.java
@@ -0,0 +1,15 @@
+package com.ali.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+
+}
